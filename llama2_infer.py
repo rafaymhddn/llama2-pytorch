5661d646fdb89708b7a26559076ec4c4643eae53
rafaymhddn <rafaymhddn@gmail.com>
2023-12-25 01:00:00 +0100

    readme updated

--- llama2_infer.py
+++ llama2_infer.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 
 from llama2 import ModelArgs, Transformer
 
-class LLaMA2:
+class LLaMA:
 
     def __init__(self, model: Transformer, 
                  tokenizer: SentencePieceProcessor, 
@@ -33,7 +33,7 @@ class LLaMA2:
             ckpt_path = checkpoints[0]
             print(f'Loading checkpoint "{ckpt_path}"')
             checkpoint = torch.load(ckpt_path, map_location="cpu")
-            print(f"Loaded checkpoint in {time.time() - prev_time:.2f}s")
+            #print(f"Loaded checkpoint in {time.time() - prev_time:.2f}s")
         
         with open(Path(checkpoints_dir) / "params.json", "r") as f:
             params = json.loads(f.read())
@@ -47,7 +47,7 @@ class LLaMA2:
 
         tokenizer = SentencePieceProcessor()
         tokenizer.Load(tokenizer_path)
-        model_args.vocab_size=max_batch_size
+        model_args.vocab_size=tokenizer.vocab_size()
 
         if device == "cuda":
             torch.set_default_tensor_type(torch.cuda.HalfTensor)
@@ -59,30 +59,33 @@ class LLaMA2:
         if load_model:
             del checkpoint['rope.freqs']
             model.load_state_dict(checkpoint, strict=True)
-            print(f"Loaded state dict in {time.time() - prev_time:.2f}s")
+            #print(f"Loaded state dict in {time.time() - prev_time:.2f}s")
 
         return LLaMA(model, tokenizer, model_args)
     
-    def text_completion(self, prompts: str, temperature: float = 0.6, top_p = 0.9, max_gen_len: Optional[int] = None):
+    def text_completion(self, prompts: list[str], temperature: float = 0.6, top_p: float = 0.9, max_gen_len: Optional[int] = None):
         if max_gen_len is None:
-            max_gen_len = self.args.max_seq_len -1
+            max_gen_len = self.args.max_seq_len - 1
+        # Convert each prompt into tokens
         prompt_tokens = [self.tokenizer.encode(prompt, out_type=int, add_bos=True, add_eos=False) for prompt in prompts]
+        # Make sure the batch size is not too large
         batch_size = len(prompt_tokens)
-        assert batch_size <= self.args.max_batch_size, f"batch size must be less <= {self.args.max_batch_size}"
+        assert batch_size <= self.args.max_batch_size, f"batch size must be less than or equal to {self.args.max_batch_size}"
         max_prompt_len = max(len(prompt) for prompt in prompt_tokens)
-        assert max_prompt_len <= self.args.max_seq_len, f"prompt length must be <= {self.args.max_seq_len}"
+        # Make sure the prompt length is not larger than the maximum sequence length
+        assert max_prompt_len <= self.args.max_seq_len, f"prompt length must be less than or equal to {self.args.max_seq_len}"
         total_len = min(self.args.max_seq_len, max_gen_len + max_prompt_len)
 
-        # List with generated tokens + initial prompt tokens
-        pad_id = self.tokenizer.pad.id()
+        # Create the list that will contain the generated tokens, along with the initial prompt tokens
+        pad_id = self.tokenizer.pad_id()
         tokens = torch.full((batch_size, total_len), pad_id, dtype=torch.long, device=device)
         for k, t in enumerate(prompt_tokens):
             # Populate the initial tokens with the prompt tokens
             tokens[k, : len(t)] = torch.tensor(t, dtype=torch.long, device=device)
-
+        
         eos_reached = torch.tensor([False] * batch_size, device=device)
-        prompt_token_mask = tokens != pad_id
-        cut_iterator = tqdm(range(1, total_len), desc="Generating tokens")
+        prompt_tokens_mask = tokens != pad_id # True if the token is a prompt token, False otherwise
+        cur_iterator = tqdm(range(1, total_len), desc="Generating tokens")
         for cur_pos in cur_iterator:
             with torch.no_grad():
                 logits = self.model.forward(tokens[:, cur_pos-1:cur_pos], cur_pos)
@@ -96,10 +99,10 @@ class LLaMA2:
 
             next_token = next_token.reshape(-1)
             # Only replace token if it is a padding token
-            next_token = torch.where(prompt_token_mask[:, cur_pos], tokens[:, cur_pos], next_token)
+            next_token = torch.where(prompt_tokens_mask[:, cur_pos], tokens[:, cur_pos], next_token)
             tokens[:, cur_pos] = next_token
             # EOS is reached only if we found an EOS token for a padding position
-            eos_reached |= (~prompt_token_mask[:, cur_pos]) & (next_token == self.tokenizer.eos_id)
+            eos_reached |= (~prompt_tokens_mask[:, cur_pos]) & (next_token == self.tokenizer.eos_id)
             if all(eos_reached):
                 break
 
@@ -135,20 +138,26 @@ class LLaMA2:
 
 
 if __name__ == '__main__':
-    torch.manual_seed(7)
+    torch.manual_seed(0)
 
     allow_cuda = False
     device = 'cuda' if torch.cuda.is_available() and allow_cuda else 'cpu'
+    
+    user_input = input("Please enter your question: ")
+    prompts = [
+        f"You are an expert AI model with knowledge of the internet and you MUST answer following question: {user_input}"
+    ]
 
-    promts =[]
-
-    model = LLaMA2.build(
+    model = LLaMA.build(
         checkpoints_dir='llama-2-7b/',
         tokenizer_path='tokenizer.model',
         load_model=True,
         max_seq_len=1024,
-        max_batch_size=20,
+        max_batch_size=len(prompts),
         device=device
     )
 
-    print("all loaded")
+    out_tokens, out_texts = (model.text_completion(prompts, max_gen_len=64))
+    assert len(out_texts) == len(prompts)
+    for i in range(len(out_texts)):
+        print(f'{out_texts[i]}')
